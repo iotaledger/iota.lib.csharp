8eb132c76a761dbf73ce4b3087a1ce1407ebbce0
saniro <sniro@stud.hs-offenburg.de>
2017-02-12 16:40:30 +0100

    Add ToString() implementation

--- IotaCSharpApi/Api/Core/GetBalancesResponse.cs
+++ IotaCSharpApi/Api/Core/GetBalancesResponse.cs
@@ -2,8 +2,6 @@
 
 namespace Iota.Lib.CSharp.Api.Core
 {
-    /// <summary>
-    /// </summary>
     public class GetBalancesResponse : IotaResponse
     {
         public List<long> Balances { get; set; }
@@ -11,5 +9,10 @@ namespace Iota.Lib.CSharp.Api.Core
         public string Milestone { get; set; }
 
         public int MilestoneIndex { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Balances)}: {Balances}, {nameof(Milestone)}: {Milestone}, {nameof(MilestoneIndex)}: {MilestoneIndex}";
+        }
     }
 }
