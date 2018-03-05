994fc476f67ea7bcc08e7cbadd7dec6a384f8ada
guojiancong <guojiancong0121@gmail.com>
2018-03-05 22:14:23 +0800

    * fomat test project

--- IotaCSharpApiUnitTests/Api/Utils/ChecksumTest.cs
+++ IotaCSharpApiUnitTests/Api/Utils/ChecksumTest.cs
@@ -6,28 +6,28 @@ namespace Iota.Lib.CSharpTests.Api.Utils
     [TestClass]
     public class ChecksumTest
     {
-        private static string TEST_ADDRESS_WITHOUT_CHECKSUM =
+        private static readonly string TEST_ADDRESS_WITHOUT_CHECKSUM =
             "RVORZ9SIIP9RCYMREUIXXVPQIPHVCNPQ9HZWYKFWYWZRE9JQKG9REPKIASHUUECPSQO9JT9XNMVKWYGVA";
 
-        private static string TEST_ADDRESS_WITH_CHECKSUM =
+        private static readonly string TEST_ADDRESS_WITH_CHECKSUM =
             "RVORZ9SIIP9RCYMREUIXXVPQIPHVCNPQ9HZWYKFWYWZRE9JQKG9REPKIASHUUECPSQO9JT9XNMVKWYGVAFOXM9MUBX";
 
         [TestMethod]
-        public void shouldAddChecksum()
+        public void ShouldAddChecksum()
         {
             Assert.AreEqual(Checksum.AddChecksum(TEST_ADDRESS_WITHOUT_CHECKSUM), TEST_ADDRESS_WITH_CHECKSUM);
         }
 
         [TestMethod]
-        public void shouldRemoveChecksum()
+        public void ShouldRemoveChecksum()
         {
-            Assert.AreEqual(Checksum.RemoveChecksum(TEST_ADDRESS_WITH_CHECKSUM), TEST_ADDRESS_WITHOUT_CHECKSUM);
+            Assert.AreEqual(TEST_ADDRESS_WITH_CHECKSUM.RemoveChecksum(), TEST_ADDRESS_WITHOUT_CHECKSUM);
         }
 
         [TestMethod]
-        public void shouldIsValidChecksum()
+        public void ShouldIsValidChecksum()
         {
-            Assert.AreEqual(Checksum.IsValidChecksum(TEST_ADDRESS_WITH_CHECKSUM), true);
+            Assert.AreEqual(TEST_ADDRESS_WITH_CHECKSUM.IsValidChecksum(), true);
         }
     }
 }
--- IotaCSharpApiUnitTests/Api/Utils/TrytesConverterTest.cs
+++ IotaCSharpApiUnitTests/Api/Utils/TrytesConverterTest.cs
@@ -8,34 +8,34 @@ namespace Iota.Lib.CSharpTests.Api.Utils
     [TestClass]
     public class TrytesConverterTest
     {
+        private static readonly Random Random = new Random();
+
         [TestMethod]
-        public void shouldConvertStringToTrytes()
+        public void ShouldConvertStringToTrytes()
         {
             Assert.AreEqual("IC", TrytesConverter.ToTrytes("Z"));
             Assert.AreEqual(TrytesConverter.ToTrytes("JOTA JOTA"), "TBYBCCKBEATBYBCCKB");
         }
 
         [TestMethod]
-        public void shouldConvertTrytesToString()
+        public void ShouldConvertTrytesToString()
         {
             Assert.AreEqual("Z", TrytesConverter.ToString("IC"));
             Assert.AreEqual(TrytesConverter.ToString("TBYBCCKBEATBYBCCKB"), "JOTA JOTA");
         }
 
-        public void shouldConvertBackAndForth()
+        public void ShouldConvertBackAndForth()
         {
-            string str = RandomString(1000);
-            string back = TrytesConverter.ToString(TrytesConverter.ToTrytes(str));
+            var str = RandomString(1000);
+            var back = TrytesConverter.ToString(TrytesConverter.ToTrytes(str));
             Assert.AreEqual(str, back);
         }
-
-        private static Random random = new Random();
 
         public static string RandomString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[random.Next(s.Length)]).ToArray());
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
         }
     }
 }
